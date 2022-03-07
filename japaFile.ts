cb13c6c31f0c81f1dbbdf0134f5ee23be8f0f0d4
ivansimplicio <ivansimplicio@outlook.com>
2022-03-07 16:13:08 -0300

    chore: japa setup

---
+++ japaFile.ts
@@ -0,0 +1,24 @@
+import getPort from 'get-port'
+import { configure } from 'japa'
+import { join } from 'path'
+import sourceMapSupport from 'source-map-support'
+
+import 'reflect-metadata'
+
+process.env.NODE_ENV = 'testing'
+process.env.ADONIS_ACE_CWD = join(__dirname)
+sourceMapSupport.install({ handleUncaughtExceptions: false })
+
+async function startHttpServer() {
+  const { Ignitor } = await import('@adonisjs/core/build/src/Ignitor')
+  process.env.PORT = String(await getPort())
+  await new Ignitor(__dirname).httpServer().start()
+}
+
+/**
+ * Configure test runner
+ */
+configure({
+  files: ['__tests__/**/*.spec.ts'],
+  before: [startHttpServer],
+})
